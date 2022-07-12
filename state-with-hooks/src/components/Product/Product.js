b21641ecedbc341a1c0cfed791384fef761a0ed9
Johnson <johnsonkarasanyi@gmail.com>
2022-07-12 14:55:45 +0200

    Setting State with useState

--- state-with-hooks/src/components/Product/Product.js
+++ state-with-hooks/src/components/Product/Product.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.css';
 
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
+
+function getTotal(total) {
+  return total.toLocaleString(undefined, currencyOptions)
+}
+
 export default function Product() {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  function add() {
+    setCart(['ice cream']);
+    setTotal(5);
+  }
   return(
     <div className="wrapper">
       <div>
-        Shopping Cart: 0 total items.
+        Shopping Cart: {cart.length} total items.
       </div>
-      <div>Total: 0</div>
+      <div>Total: {getTotal(total)}</div>
 
       <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-      <button>Add</button> <button>Remove</button>
+      <button onClick={add}>Add</button>
+      <button
+        onClick={() => {
+          setCart([]);
+          setTotal(0);
+        }}
+      >
+        Remove
+      </button>
     </div>
   )
 }
