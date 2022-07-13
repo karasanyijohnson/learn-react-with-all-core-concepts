1a2d0a1a963c20f7966e6cd0d9952f2295367ee0
Johnson <johnsonkarasanyi@gmail.com>
2022-07-13 10:27:39 +0200

    Setting State Using Current State

--- state-with-hooks/src/components/Product/Product.js
+++ state-with-hooks/src/components/Product/Product.js
@@ -1,40 +1,84 @@
-import React, { useState } from 'react';
+import React, {useReducer} from 'react';
 import './Product.css';
 
+const products = [
+  {
+    emoji: '🍦',
+    name: 'ice cream',
+    price: 5
+  },
+  {
+    emoji: '🍩',
+    name: 'donuts',
+    price: 2.5,
+  },
+  {
+    emoji: '🍉',
+    name: 'watermelon',
+    price: 4
+  }
+];
+function cartReducer(state, action) {
+  switch(action.type) {
+    case 'add':
+      return [...state, action.product];
+    case 'remove':
+      const productIndex = state.findIndex(item => item.name === action.product.name);
+      if(productIndex < 0) {
+        return state;
+      }
+      const update = [...state];
+      update.splice(productIndex, 1)
+      return update;
+    default:
+      return state;
+  }
+}
+function totalReducer(state, action) {
+  if(action.type === 'add') {
+    return state + action.price;
+  }
+  return state - action.price
+}
 const currencyOptions = {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 }
 
-function getTotal(total) {
+function getTotal(cart) {
+  const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
   return total.toLocaleString(undefined, currencyOptions)
 }
 
 export default function Product() {
-  const [cart, setCart] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useReducer(cartReducer, []);
+  // const [total, setTotal] = useState(0);
+  const [total, setTotal] = useReducer(totalReducer, 0);
 
-  function add() {
-    setCart(['ice cream']);
-    setTotal(5);
+  function add(product) {
+    setCart({ product, type: 'add' });
   }
+  function remove(product) {
+    setCart({ product, type: 'remove' });
+  }
+
   return(
     <div className="wrapper">
       <div>
         Shopping Cart: {cart.length} total items.
       </div>
-      <div>Total: {getTotal(total)}</div>
-
-      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-      <button onClick={add}>Add</button>
-      <button
-        onClick={() => {
-          setCart([]);
-          setTotal(0);
-        }}
-      >
-        Remove
-      </button>
+      <div>Total: {getTotal(cart)}</div>
+      <div>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+            <button onClick={() => add(product)}>Add</button>
+            <button onClick={() => remove(product)}>Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
