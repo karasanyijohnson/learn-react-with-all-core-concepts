4dc038715f484f56c3b92342aee09e28659a4d9f
Johnson <johnsonkarasanyi@gmail.com>
2022-07-14 10:22:56 +0200

    initial commit

---
+++ share-state-across-react-components-with-context/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
