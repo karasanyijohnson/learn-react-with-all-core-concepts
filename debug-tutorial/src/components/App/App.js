ac78c1b5ba8f3a960d257ddd253f7ccdaf29bcc9
Johnson <johnsonkarasanyi@gmail.com>
2022-07-15 12:45:37 +0200

    explore the component tree

--- debug-tutorial/src/components/App/App.js
+++ debug-tutorial/src/components/App/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
-
+import TextInformation from '../TextInformation/TextInformation';
+export const TextContext = createContext();
+TextContext.displayName = 'TextContext';
 function App() {
+  const [text, setText] = useState('');
+
   return(
-    <div className="wrapper">
-      
-     <label htmlFor="text">
-       Add Your Text Here:
-       <br></br>
-       <textarea
-         id="text"
-         name="text"
-         rows="10"
-         cols="100"
-       >
-       </textarea>
-      </label>
-    </div>
+    <TextContext.Provider value={text}>
+      <div className="wrapper">
+        <label htmlFor="text">
+          Add Your Text Here:
+          <br></br>
+          <textarea
+            id="text"
+            name="text"
+            rows="10"
+            cols="100"
+            onChange={e => setText(e.target.value)}
+          >
+          </textarea>
+        </label>
+        <TextInformation/>
+      </div>
+    </TextContext.Provider>
   )
 }
 
