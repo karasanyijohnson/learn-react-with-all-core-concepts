950edcb5e9bfb36fd711ce756e2943cafae80c62
Johnson <johnsonkarasanyi@gmail.com>
2022-07-18 13:38:26 +0200

    Adding Window Events

--- events-tutorial/src/components/FileNamer/FileNamer.js
+++ events-tutorial/src/components/FileNamer/FileNamer.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './FileNamer.css';
 
 export default function FileNamer() {
   const [name, setName] = useState('');
   const [alert, setAlert] = useState(false);
+  useEffect(() => {
+    const handleWindowClick = () => setAlert(false)
+    if(alert) {
+      window.addEventListener('click', handleWindowClick);
+    } else {
+      window.removeEventListener('click', handleWindowClick)
+    }
+    return () => window.removeEventListener('click', handleWindowClick);
+  }, [alert, setAlert]);
+
   const validate = event => {
     if(/\*/.test(name)) {
       event.preventDefault();
@@ -13,32 +23,66 @@ export default function FileNamer() {
       setAlert(false);
  };
   return(
+    // <div className="wrapper">
+    //   <div className="preview">
+    //     <h2>Preview: {name}.js</h2>
+    //   </div>
+    //   <form>
+    //     <label>
+    //       <p>Name:</p>
+    //       <input
+    //         autoComplete="off"
+    //         name="name"
+    //         onBlur={() => setAlert(false)}
+    //         onChange={event => setName(event.target.value) }
+    //         onFocus={() => setAlert(true)}
+    //       />
+    //     </label>
+    //     {alert &&
+    //      <div>
+    //        <span role="img" aria-label="allowed">✅</span> Alphanumeric Characters
+    //        <br />
+    //        <span role="img" aria-label="not allowed">⛔️</span> *
+    //      </div>
+    //    }
+    //     <div>
+    //     <button onClick={validate}>Save</button>
+    //     </div>
+    //   </form>
+    // </div>
     <div className="wrapper">
-      <div className="preview">
-        <h2>Preview: {name}.js</h2>
-      </div>
-      <form>
-        <label>
-          <p>Name:</p>
-          <input
-            autoComplete="off"
-            name="name"
-            onBlur={() => setAlert(false)}
-            onChange={event => setName(event.target.value) }
-            onFocus={() => setAlert(true)}
-          />
-        </label>
-        {alert &&
-         <div>
+    <div className="preview">
+      <h2>Preview: {name}.js</h2>
+    </div>
+    <form>
+      <label>
+        <p>Name:</p>
+        <input
+          autocomplete="off"
+          name="name"
+          onChange={event => setName(event.target.value) }
+        />
+      </label>
+      <div className="information-wrapper">
+        <button
+          className="information"
+          onClick={() => setAlert(true)}
+          type="button"
+        >
+          more information
+        </button>
+       {alert &&
+         <div className="popup">
            <span role="img" aria-label="allowed">✅</span> Alphanumeric Characters
            <br />
            <span role="img" aria-label="not allowed">⛔️</span> *
          </div>
        }
-        <div>
+      </div>
+      <div>
         <button onClick={validate}>Save</button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </form>
+  </div>
   )
 }
