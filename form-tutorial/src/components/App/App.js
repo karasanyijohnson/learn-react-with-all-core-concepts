2e9c78dafa5d4144108c079b536e43e8a490a835
Johnson <johnsonkarasanyi@gmail.com>
2022-07-19 17:19:23 +0200

    learned how to create uncontrolled form components

--- form-tutorial/src/components/App/App.js
+++ form-tutorial/src/components/App/App.js
@@ -1,30 +1,71 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+ return {
+   ...state,
+  [event.name]: event.value
+ }
+}
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
   const handleSubmit = event => {
     event.preventDefault();
     setSubmitting(true);
+
     setTimeout(() => {
       setSubmitting(false);
     }, 3000)
   }
- 
-  return (
+  const handleChange = event => {
+    const isCheckbox = event.target.type === 'checkbox';
+    setFormData({
+      name: event.target.name,
+      value: isCheckbox ? event.target.checked : event.target.value,
+    });
+  }
+  return(
     <div className="wrapper">
       <h1>How About Them Apples</h1>
       {submitting &&
-       <div>Submtting Form...</div>
-     }
+       <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
       <form onSubmit={handleSubmit}>
-      <fieldset>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+        </fieldset>
+        <fieldset>
          <label>
-           <p>Name</p>
-           <input name="name" />
+           <p>Apples</p>
+           <select name="apple" onChange={handleChange}>
+               <option value="">--Please choose an option--</option>
+               <option value="fuji">Fuji</option>
+               <option value="jonathan">Jonathan</option>
+               <option value="honey-crisp">Honey Crisp</option>
+           </select>
+         </label>
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" onChange={handleChange} step="1"/>
+         </label>
+         <label>
+           <p>Gift Wrap</p>
+           <input type="checkbox" name="gift-wrap" onChange={handleChange} />
          </label>
        </fieldset>
-       <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
