ff05d5f19f575b1d6ab05ffd55a50fc9cc9e7dfa
Johnson <johnsonkarasanyi@gmail.com>
2022-07-20 10:08:16 +0200

    asynch tutorial

---
+++ async-tutorial/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import RiverInformation from '../RiverInformation/RiverInformation';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>World's Longest Rivers</h1>
+      <RiverInformation />
+    </div>
+  );
+}
+
+export default App;
