f9ef840d06f4e9e7d8004686a160443b69c8ec42
Johnson <johnsonkarasanyi@gmail.com>
2022-07-21 09:21:07 +0200

    sending data to API

--- api-tutorial/src/App/App.js
+++ api-tutorial/src/App/App.js
@@ -1,28 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { getList } from '../services/list';
+import { getList, setItem } from '../services/list';
 
 function App() {
+  const [itemInput, setItemInput] = useState('');
   const [list, setList] = useState([]);
 
   useEffect(() => {
-   let mounted = true;
-   getList()
-     .then(items => {
-       if(mounted) {
-         setList(items)
-       }
-     })
-   return () => mounted = false;
- }, [])
+    let mounted = true;
+    getList()
+      .then(items => {
+        if(mounted) {
+          setList(items)
+        }
+      })
+    return () => mounted = false;
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItem(itemInput)
+  };
 
   return(
     <div className="wrapper">
-     <h1>My Grocery List</h1>
-     <ul>
-       {list.map(item => <li key={item.item}>{item.item}</li>)}
-     </ul>
-   </div>
+      <h1>My Grocery List</h1>
+      <ul>
+        {list.map(item => <li key={item.item}>{item.item}</li>)}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>New Item</p>
+          <input type="text" onChange={event => setItemInput(event.target.value)} value={itemInput} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
