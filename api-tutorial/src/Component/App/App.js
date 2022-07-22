7b3d7c3c5f0f335bbd711f14d29d19c1f12a85d4
Johnson <johnsonkarasanyi@gmail.com>
2022-07-22 11:25:21 +0200

    Preventing Updates on Unmounted Components

--- api-tutorial/src/Component/App/App.js
+++ api-tutorial/src/Component/App/App.js
@@ -1,30 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { getList, setItem } from '../services/list';
 
 function App() {
+  const [alert, setAlert] = useState(false);
   const [itemInput, setItemInput] = useState('');
   const [list, setList] = useState([]);
+  const mounted = useRef(true);
 
   useEffect(() => {
-    let mounted = true;
+    mounted.current = true;
+    if(list.length && !alert) {
+      return;
+    }
     getList()
       .then(items => {
-        if(mounted) {
+        if(mounted.current) {
           setList(items)
         }
       })
-    return () => mounted = false;
-  }, [])
+    return () => mounted.current = false;
+  }, [alert, list])
+
+  useEffect(() => {
+    if(alert) {
+      setTimeout(() => {
+        if(mounted.current) {
+          setAlert(false);
+        }
+      }, 1000)
+    }
+  }, [alert])
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setItem(itemInput)
+      .then(() => {
+        if(mounted.current) {
+          setItemInput('');
+          setAlert(true);
+        }
+      })
   };
 
   return(
     <div className="wrapper">
       <h1>My Grocery List</h1>
+      {alert && <h2> Submit Successful</h2>}
       <ul>
         {list.map(item => <li key={item.item}>{item.item}</li>)}
       </ul>
