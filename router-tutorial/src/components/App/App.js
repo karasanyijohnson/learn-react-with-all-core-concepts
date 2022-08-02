8329f626c0ed86a8f80c60533a83bbff219df095
Johnson <johnsonkarasanyi@gmail.com>
2022-08-02 12:41:55 +0200

    created a route for each component and added a navigation using the Link component to switch between routes without a page refresh

--- router-tutorial/src/components/App/App.js
+++ router-tutorial/src/components/App/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import Manatee from '../Manatee/Manatee';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Manatee from '../Manatee/Manatee';
+import Narwhal from '../Narwhal/Narwhal';
+import Whale from '../Whale/Whale';
+
 function App() {
   return (
     <div className="wrapper">
       <h1>Marine Mammals</h1>
-      <Manatee/>
+      {/* Contents out side BrowserRouter will appear in all pages */}
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/manatee">Manatee</Link></li>
+            <li><Link to="/narwhal">Narwhal</Link></li>
+            <li><Link to="/whale">Whale</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/manatee">
+            <Manatee />
+          </Route>
+          <Route path="/narwhal">
+            <Narwhal />
+          </Route>
+          <Route path="/whale">
+            <Whale />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
