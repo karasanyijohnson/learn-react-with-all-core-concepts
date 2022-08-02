4de726b50b7cb2eb8e67df585e18b036795c8a91
Johnson <johnsonkarasanyi@gmail.com>
2022-08-02 11:25:31 +0200

    installed React Router and created basic components

---
+++ router-tutorial/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Manatee from '../Manatee/Manatee';
+import './App.css';
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <Manatee/>
+    </div>
+  );
+}
+
+export default App;
