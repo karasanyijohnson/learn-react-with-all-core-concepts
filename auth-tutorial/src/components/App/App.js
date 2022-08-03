16d8c4e4d4b35f367ddc101740b9ce7fdf40e29a
Johnson <johnsonkarasanyi@gmail.com>
2022-08-03 12:00:44 +0200

    Created Login page

--- auth-tutorial/src/components/App/App.js
+++ auth-tutorial/src/components/App/App.js
@@ -1,21 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from '../Login/Login';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={ <Dashboard />}/>
+          <Route path="/preferences" element={ <Preferences />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
