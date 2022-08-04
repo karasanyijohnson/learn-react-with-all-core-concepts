32098c17022fdd34e5a7e0d76523ce78629c9a59
Johnson <johnsonkarasanyi@gmail.com>
2022-08-04 10:01:56 +0200

    custom Hook to store your token in sessionStorage

--- auth-tutorial/src/components/App/App.js
+++ auth-tutorial/src/components/App/App.js
@@ -4,19 +4,11 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from '../Login/Login';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
 
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
   if(!token) {
     return <Login setToken={setToken} />
   }
