9fd4ab0e38867c2df1bbb47320a284183a43c62b
Johnson <johnsonkarasanyi@gmail.com>
2022-08-05 10:59:35 +0200

    Testing Performance

---
+++ performance-tutorial/src/components/App/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import CharacterMap from '../CharacterMap/CharacterMap';
+
+function App() {
+  const [text, setText] = useState('');
+
+  return(
+    <div className="wrapper">
+      <label htmlFor="text">
+        <p>Your Text</p>
+        <textarea
+          id="text"
+          name="text"
+          rows="10"
+          cols="100"
+          onChange={event => setText(event.target.value)}
+        >
+        </textarea>
+      </label>
+      <CharacterMap text={text} />
+    </div>
+  )
+}
+
+export default App;
