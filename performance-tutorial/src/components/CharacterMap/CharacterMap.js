30654e27957ef8ed8a4d71c87f7e1b167df1203b
Johnson <johnsonkarasanyi@gmail.com>
2022-08-11 11:08:52 +0200

    make small changes in JSX

--- performance-tutorial/src/components/CharacterMap/CharacterMap.js
+++ performance-tutorial/src/components/CharacterMap/CharacterMap.js
@@ -1,11 +1,11 @@
 import React, { memo, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
-function itemize(text){
+function itemize(text,transformer){
   const letters = text.split('')
     .filter(l => l !== ' ')
     .reduce((collection, item) => {
-      const letter = item.toLowerCase();
+      const letter = transformer ? transformer(item) : item;
       return {
         ...collection,
         [letter]: (collection[letter] || 0) + 1
@@ -15,28 +15,33 @@ function itemize(text){
     .sort((a, b) => b[1] - a[1]);
 }
 
-function CharacterMap({ showExplanation, text }) {
-    const characters = useMemo(() => itemize(text), [text]);
-    return(
-      <div>
-        {showExplanation &&
-          <p>
-            This display a list of the most common characters.
-          </p>
-        }
-        Character Map:
-        {characters.map(character => (
+function CharacterMap({ showExplanation, text, transformer }) {
+    const characters = useMemo(() => itemize(text, transformer), [text, transformer]);
+  return(
+    <div>
+      {showExplanation &&
+        <p>
+          This display a list of the most common characters.
+        </p>
+      }
+      Character Map:
+      {characters.map(character => (
         <div key={character[0]}>
           {character[0]}: {character[1]}
         </div>
       ))}
-      </div>
-    )
-  }
-  
-  CharacterMap.propTypes = {
-    showExplanation: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-  }
-  
-  export default memo(CharacterMap);
+    </div>
+  )
+}
+
+CharacterMap.propTypes = {
+  showExplanation: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
+  transformer: PropTypes.func
+}
+
+CharacterMap.defaultProps = {
+  transformer: null
+}
+
+export default memo(CharacterMap);
